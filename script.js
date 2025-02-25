90a8179672a133f8695ea691d21c5d69441fa819
mjtahia.tm10 <mjtahiaa@gmail.com>
2025-02-25 22:02:23 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -13,19 +13,57 @@ copyDiscountCoupon.addEventListener('click', function(){
         copyDiscountCoupon.textContent = discountText;
 
     }, 900);
- })
- let itemList = [] ;
- function AddItem(item,price){
-    itemList.push(item);
-    renderList() ;
- }
+ });
  
+ const couponButton = document.getElementById('coupon-button');
+ const purchaseButton = document.getElementById('purchase-button');
+ const totalPriceElement = document.getElementById('total-price');
+ const discountElement = document.getElementById('total-discount');
+ const finalPriceElement = document.getElementById('price-after-discount');
+ const cartItemContainer = document.getElementById('cart-item');
+
+ let itemList = [];
+ let totalPrice = 0;
+ let discount = 0;
+  
+ couponButton.disabled = true ;
+ purchaseButton.disabled = true;
+
+ function addItem( item,price) {
+    itemList.push({ name: item, price: price})
+    totalPrice += price;
+    updatePrice();
+ }
+
+ function removeItem(index) {
+    if (index >= 0 && index < itemList.length ) {
+        totalPrice -= itemList[index].price;
+        itemList.splice( index,1);
+        updatePrice()
+    }
+
+ }
+
+ function updatePrice(){
+    totalPriceElement.innerText = totalPrice.toFixed(2);
+    discount = 0;
+    discountElement.innerText = discount.toFixed(2);
+    finalPriceElement.innerText = (totalPrice-discount).toFixed(2)
+    purchaseButton.disabled = totalPrice === 0;
+    couponButton.disabled = totalPrice  < 3000;
+    renderList()
+ }
  function renderList(){
-    let output = document.getElementById('cart-item')
-    output.innerHTML= '' ;
-    itemList.forEach((item,index)  =>{
-output.innerHTML += ` <div>
-${index + 1} . <sapn> ${item}</sapn>
-</div>`
+    output.innerHTML = '';
+    itemList.forEach((item,index) => {
+        output.innerHTML += `
+        <div class=" flex items-center justify-between"> 
+        <span>${index + 1}. ${item.name}</span>
+        <span> 
+        <i onclick="removeItem(${index}) class=" fa-solid" ">
+        </i>
+        </span>
+        </div>`
     })
  }
+
